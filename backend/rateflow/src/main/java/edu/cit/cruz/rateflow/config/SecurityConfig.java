47bb9c0a09a914b770337287ab057a5b4cda7adb
Harley Cruz <johnharleycruz592@gmail.com>
2026-04-12 20:34:41 +0800

    updated ports

--- backend/rateflow/src/main/java/edu/cit/cruz/rateflow/config/SecurityConfig.java
+++ backend/rateflow/src/main/java/edu/cit/cruz/rateflow/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://10.0.2.2:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://10.0.2.2:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
